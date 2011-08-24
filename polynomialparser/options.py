07c438fcdd48324235eec327bf75b355188a4283
Chris <archgoon@gmail.com>
2011-08-23 17:21:12 -0700

    Options parsing.

---
+++ polynomialparser/options.py
@@ -0,0 +1,14 @@
+import optparse
+
+def parse_options(args):
+	parser = optparse.OptionParser()
+	parser.add_option("-x",dest="expression",
+			help="directly evaluate expression",
+			default=None)
+	parser.add_option("-f",dest="filename",
+			help="filename containing list of expressions "+
+			"if not specified, STDIN is used",
+			default=None)
+	parser.add_option("-e",action="store_true",dest="eval",default=False)
+	options,args = parser.parse_args()
+	return options,args
