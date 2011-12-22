268424635ca9c8affe6b772eef7c25b53c050d79
Chris Greene <chris.greene@justin.tv>
2011-12-22 12:38:46 -0800

    Added more comments

--- math/partitions.hs
+++ math/partitions.hs
@@ -7,13 +7,13 @@ increment p = []                --Don't create a new one, there will be only one
 
 partition_successor p = [([1]++p)] ++ (increment p)    --Insert 1 to partition, and maybe also increment
 
-partitions :: Num a=>Ord a => a -> [[a]]
-partitions 0 = [[]]
-partitions n = concat $ map partition_successor (partitions (n-1))
+partitions :: Num a=>Ord a => a -> [[a]]                           --Type signature
+partitions 0 = [[]]                                                --Base case
+partitions n = concat $ map partition_successor (partitions (n-1)) --For each partition for the number n-1, we construct the successors
 
 main = do 
         args <- getArgs
         let n = (read (head args) :: Int)
-        let k = partitions n
-        putStrLn $ show (length k)
+        let parts = partitions n
+        putStrLn $ show (length parts)
         
